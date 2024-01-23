e257ff0d5179bf99dcaf4febff4017ac4413ed83
ecomnazar <ecomnazar.me@gmail.com>
2024-01-23 16:51:25 +0500

    [fixed]: any bugs

--- src/student/queries.js
+++ src/student/queries.js
@@ -1,12 +1,11 @@
 const getStudents = "SELECT * FROM users";
 const getStudentById = "SELECT * FROM users WHERE id = $1";
-const getStudentByEmail = "SELECT * FROM users WHERE email = $1";
 const checkEmailExists = "SELECT * FROM users WHERE email = $1";
 const addStudent =
   "INSERT INTO users (fullname, email, password, region, gender, certificate_key, date) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *";
 const removeStudent = "DELETE FROM users WHERE id = $1";
 const updateStudent =
-  "UPDATE users SET fullname = $1, email = $2, region = $3, gender = $4 WHERE email = $2";
+  "UPDATE users SET fullname = $1, email = $2, region = $3, gender = $4 WHERE email = $2 RETURNING *";
 
 module.exports = {
   getStudents,
@@ -15,5 +14,4 @@ module.exports = {
   addStudent,
   removeStudent,
   updateStudent,
-  getStudentByEmail,
 };
