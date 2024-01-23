f837e6fe6f1586ec38a8b0f3e4c2a59004dc8f9a
ecomnazar <ecomnazar.me@gmail.com>
2024-01-23 15:03:31 +0500

    [added]: login api

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 const Pool = require("pg").Pool;
 
-// const pool = new Pool({
-//   host: "localhost",
-//   user: "postgres",
-//   password: "postgres",
-//   database: "mhpss",
-//   port: 5432,
-// });
-
 const pool = new Pool({
   host: "localhost",
-  user: "seven",
-  password: "post",
-  database: "students",
+  user: "postgres",
+  password: "postgres",
+  database: "mhpss",
   port: 5432,
 });
+
+// const pool = new Pool({
+//   host: "localhost",
+//   user: "seven",
+//   password: "post",
+//   database: "students",
+//   port: 5432,
+// });
 
 module.exports = pool;
